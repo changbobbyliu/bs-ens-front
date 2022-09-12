e5d5006da1f9ec2010be6f95d3ec9ce1f79c014f
Chang Liu <changbobbyliu@gmail.com>
2022-09-11 18:08:47 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-800 text-white">
+      <header className="App-header h-16"></header>
+      <main className="flex flex-col items-center justify-center">
+        <h1 className="font-bold text-xl">📍 Fake ETH Name Service</h1>
+        <p>This seems like an ENS project clone!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
