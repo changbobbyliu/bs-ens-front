fec82f67dd42802c21dc9bcb0d208ac3dc1e514a
Chang Liu <changbobbyliu@gmail.com>
2022-09-14 03:46:17 -0700

    add set record

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 1. simplyanother.eth: https://mumbai.polygonscan.com/tx/0x211f9cbfbce3d7ca1f6e3484d2216a51280a6a105c89c04c41405adad88d6821
    record: https://mumbai.polygonscan.com/tx/0xbc084f1c3a9ea19a2c761dcef3cf7150fa3e9157d46960aaed29da459ac43d3e
+
+## Gas report
+
+setRecore(0123456789) -> 1/0.00008099 = 12,347.20
--- src/components/MintDomainForm.component.tsx
+++ src/components/MintDomainForm.component.tsx
@@ -1,10 +1,17 @@
 import { ethers } from "ethers";
 import { useState } from "react";
 import { C } from "../config/constants";
+import { useGContext } from "../GContext";
+import { useFetchMints } from "../hooks/contract/useFetchMints";
 
 export const MintDomainForm = () => {
+  const { mints, walletAddress } = useGContext();
+  const { fetchMints } = useFetchMints();
+
   const [domain, setDomain] = useState("");
   const [record, setRecord] = useState("");
+  const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const mintDomain = async () => {
     if (!domain || !record || domain.length < 3) {
@@ -43,6 +50,8 @@ export const MintDomainForm = () => {
 
         console.log("Record set! https://mumbai.polygonscan.com/tx/" + tx.hash);
 
+        setTimeout(fetchMints, 2000);
+
         setRecord("");
         setDomain("");
       } else {
@@ -51,6 +60,85 @@ export const MintDomainForm = () => {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const updateDomain = async () => {
+    if (!record || !domain) {
+      return;
+    }
+    setLoading(true);
+    console.log("Updating domain", domain, "with record", record);
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          C.CONTRACT_ADDRESS,
+          C.CONTRACT_ABI.abi,
+          signer
+        );
+
+        let tx = await contract.setRecord(domain, record);
+        await tx.wait();
+        console.log("Record set https://mumbai.polygonscan.com/tx/" + tx.hash);
+
+        fetchMints();
+        setRecord("");
+        setDomain("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  const renderMints = () => {
+    if (mints.length > 0) {
+      return (
+        <div className="my-4">
+          <p className="text-center my-2 font-mono uppercase">
+            Recently minted domains!
+          </p>
+          <div className="grid grid-cols-2 gap-2">
+            {mints.map((mint, index) => {
+              return (
+                <div className="p-2 rounded-sm bg-slate-400" key={index}>
+                  <div className="flex justify-between items-center">
+                    <a
+                      href={`https://testnets.opensea.io/assets/mumbai/${C.CONTRACT_ADDRESS}/${mint.id}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <p className="underline">{`${mint.name}.${C.tld}`}</p>
+                    </a>
+                    {/* If mint.owner is currentAccount, add an "edit" button*/}
+                    {mint.owner.toLowerCase() ===
+                    walletAddress?.toLowerCase() ? (
+                      <button
+                        className="w-4 h-4 opacity-80"
+                        onClick={() => editRecord(mint.name)}
+                      >
+                        <img
+                          src="https://img.icons8.com/metro/26/000000/pencil.png"
+                          alt="Edit button"
+                        />
+                      </button>
+                    ) : null}
+                  </div>
+                  <p className="mt-2">{mint.record}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    }
+  };
+
+  const editRecord = (name: string) => {
+    console.log("Editing record for", name);
+    setEditing(true);
+    setDomain(name);
   };
 
   return (
@@ -74,15 +162,35 @@ export const MintDomainForm = () => {
         className="bg-gray-700 text-white px-4 py-2 rounded mt-4"
       />
 
-      <div>
-        <button
-          className="bg-red-500 text-white px-4 py-2 rounded mt-4 ml-auto block"
-          disabled={false}
-          onClick={mintDomain}
-        >
-          Mint
-        </button>
+      <div className="flex justify-end">
+        {editing ? (
+          <>
+            <button
+              onClick={updateDomain}
+              className="bg-amber-500 text-white px-4 py-2 rounded mt-4 block"
+            >
+              Set Record
+            </button>
+            <button
+              className="bg-red-500 text-white px-4 py-2 rounded mt-4 ml-2 block"
+              onClick={() => {
+                setEditing(false);
+              }}
+            >
+              Cancel
+            </button>
+          </>
+        ) : (
+          <button
+            className="bg-amber-500 text-white px-4 py-2 rounded mt-4 block"
+            disabled={false}
+            onClick={mintDomain}
+          >
+            Mint
+          </button>
+        )}
       </div>
+      {renderMints()}
     </div>
   );
 };
